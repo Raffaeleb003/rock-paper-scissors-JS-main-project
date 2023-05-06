a6b1662065fb828def8782464a379b3dab34df4b
Raffaele Balsamo <80632324+Raffaeleb003@users.noreply.github.com>
2023-05-06 15:38:17 +0200

    final commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const computerPlay = () => choices[Math.floor(Math.random() * choices.length)];
 const playRound = (playerSelection, computerSelection) => {
   playerSelection = playerSelection.toLowerCase();
   if (!choices.includes(playerSelection)) {
-    alert(`Invalid choice: ${playerSelection}.`);
+    return `Invalid choice: ${playerSelection}.`;
   }
 
   if (playerSelection === computerSelection) {
@@ -25,29 +25,26 @@ const game = () => {
   let playerScore = 0;
   let computerScore = 0;
   for (let i = 0; i < 5; i++) {
-    let playerSelection = "";
-    while (!choices.includes(playerSelection)) {
-      playerSelection = prompt(`Round ${i+1}: Enter your choice: ${choices.join(", ")}`).toLowerCase();
-      try {
-        playRound(playerSelection, computerPlay());
-      } catch (e) {
-        alert(e.message);
+    let playerSelection = null;
+    while (playerSelection === null || !choices.includes(playerSelection)) {
+      playerSelection = prompt(`Round ${i+1}: Enter your choice: ${choices.join(", ")}`);
+      if (playerSelection === null) {
+        alert("You must play the round. Please enter your choice.");
+      } else {
+        playerSelection = playerSelection.toLowerCase();
+        if (!choices.includes(playerSelection)) {
+          alert(`Invalid choice: ${playerSelection}. Please choose from ${choices.join(", ")}.`);
+        }
       }
     }
     const computerSelection = computerPlay();
-      if (playerSelection) {
-      try {
-        const result = playRound(playerSelection, computerSelection);
-        console.log(result);
+    const result = playRound(playerSelection, computerSelection);
+    console.log(result);
 
-      if (result.startsWith("You win!")) {
-          playerScore++;
-        } else if (result.startsWith("You lose!")) {
-          computerScore++;
-        }
-  } catch (e) {
-        alert(e.message);
-      }
+    if (result.startsWith("You win!")) {
+      playerScore++;
+    } else if (result.startsWith("You lose!")) {
+      computerScore++;
     }
   }
 
@@ -61,4 +58,3 @@ const game = () => {
 };
 
 game()
-
